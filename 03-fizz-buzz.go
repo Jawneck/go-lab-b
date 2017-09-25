4ee2a4f9a99bfd2b0b71699243d3b8f082402484
Danielis Joniskis <g00333859@gmit.ie>
2017-09-25 20:38:11 +0100

    Completed fizzbuzz

--- 03-fizz-buzz.go
+++ 03-fizz-buzz.go
@@ -9,6 +9,17 @@ package main
 import "fmt"
 
 func main() {
-
-
+	//Opening the for loop
+	for i := 0; i <= 100; i++{
+		//Checking if number i is divisable by 15, if it is we dont check the other two conditions.
+		if(i % 15 == 0){
+			fmt.Println("Fizz" + "Buzz")
+		} else if (i % 3  == 0) {
+			fmt.Println ("Fizz")
+		} else if (i % 5 == 0){
+			fmt.Println ("Buzz")
+		} else{
+			fmt.Println(i)
+		}		
+	}
 }//end main
