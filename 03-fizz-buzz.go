a078ebc26eef729422f05d2be2dd0c4b313749e4
DANIELIS JONISKIS - Student <G00333859@gmit.ie>
2017-09-25 09:56:20 +0100

    Added fizzbuzz

---
+++ 03-fizz-buzz.go
@@ -0,0 +1,13 @@
+//A program that prints the numbers from 1 to 100, each on a new line, to the console, except for the following conditions. 
+//For multiples of three print Fizz instead of the number, and for multiples of five print Buzz. 
+//For numbers that are multiples of both three and five print FizzBuzz.
+//Author: Danielis Joniskis
+//Adapted from http://wiki.c2.com/?FizzBuzzTest
+package main
+
+import "fmt"
+
+func main() {
+
+
+}//end main
