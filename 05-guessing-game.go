09f2a91f51885533834803e667dbe91e991636ad
Danielis Joniskis <g00333859@gmit.ie>
2017-10-02 23:53:20 +0100

    added 05-guessing-game

---
+++ 05-guessing-game.go
@@ -0,0 +1,5 @@
+//A program where the user must guess a randomly generated number.
+//After every guess the program tells the user whether their number was too high or too low.
+//At the end, the number of tries should be printed. 
+//It counts only as one try if they input the same number multiple times consecutively.
+//Author: Danielis Joniskis
