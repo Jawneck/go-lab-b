2035f73bd84a12e3572ab929857316e36b8e9595
Danielis Joniskis <g00333859@gmit.ie>
2017-10-02 22:18:08 +0100

    started factorial

---
+++ 04-factorial-digit-sum.go
@@ -0,0 +1,11 @@
+// A program that calculates the sum of the digits of the factorial of a number.
+//Author: Danielis Joniskis
+
+package main
+
+//fmt is Go’s standard Input/Output library.
+import "fmt"
+
+func main() {
+
+}
