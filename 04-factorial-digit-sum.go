07555216afee03a8b5099c3ad60ae1f9f68e7be6
Danielis Joniskis <g00333859@gmit.ie>
2017-10-02 23:49:24 +0100

    delete

--- 04-factorial-digit-sum.go
+++ 04-factorial-digit-sum.go
@@ -3,23 +3,4 @@
 
 package main
 //fmt is Go’s standard Input/Output library.
-func P20() int {
-    sum := 0;
-    digits := [200]int{};
-    digits[0] = 1;
-    for i := 2; i <= 100; i++ {
-    	for j := 0; j < len(digits); j++ {
-    		digits[j] *= i;
-    		if j > 0 && digits[j - 1] > 9 {
-    			digits[j] += int(digits[j - 1] / 10);
-    			digits[j - 1] %= 10;
-    		}
-    	}
-    }
-    for i := 0; i < len(digits); i++ {
-    	sum += digits[i];
-    }
-    return sum;
-}
-
 
