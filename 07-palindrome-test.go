9a3e5fe0e508ebd2b290cad52b985a48e15ae589
Danielis Joniskis <g00333859@gmit.ie>
2017-10-04 17:05:55 +0100

    added Palindrome Test

---
+++ 07-palindrome-test.go
@@ -0,0 +1,12 @@
+//a function that tests whether a string is a palindrome. 
+//A palindrome is a string that reads the same in reverse, e.g. radar.
+//Author: Danielis Joniskis
+
+package main
+
+//fmt is Go’s standard Input/Output library.
+import "fmt"
+
+func main(){
+
+}
