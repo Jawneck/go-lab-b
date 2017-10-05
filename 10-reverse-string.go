d0d905d4583594d0e2342af1fb7c1079c3678e6b
Danielis Joniskis <g00333859@gmit.ie>
2017-10-05 17:03:00 +0100

    added the reverse string function

---
+++ 10-reverse-string.go
@@ -0,0 +1,23 @@
+//Write a function to reverse a string in go.
+//Author: Danielis Joniskis
+
+package main
+
+//fmt is Go’s standard Input/Output library.
+import "fmt"
+
+//We call the reverse function in the main method.
+func main(){
+	s := "problem sheet"
+	fmt.Println(s)
+
+	fmt.Println(reverse(s))
+}
+//A function which reverses a string.
+func reverse(s string) string{
+	var reverse string
+	for i := len(s) - 1; i >= 0; i--{
+		reverse += string (s[i])
+	}
+	return reverse
+}
