281abdf304dfb99089eeb61c83e0a0ab81354937
Danielis Joniskis <g00333859@gmit.ie>
2017-10-05 14:29:26 +0100

    appending two lists

--- 08-merge-list-sort.go
+++ 08-merge-list-sort.go
@@ -6,12 +6,13 @@ package main
 
 //fmt is Go’s standard Input/Output library.
 import "fmt"
-
-//A merge sort is a divide and conquer algorithm.
-//It splits the input into chunks, sorts them and then merges them together.
+//sort provides primitives for sorting slices and user-defined collections
+//import "sort"
 
 func main(){
-	x := []int{
-		30, 88, -10, 24, 12, 5, 64, 33,  
-	}
+
+	slice := append([]int {1, 4, 6}, []int {2, 3, 5}...)
+
+	fmt.Println(slice)
 }
+
