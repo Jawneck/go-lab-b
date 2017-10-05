7826c86f460c75b914037def505a5a90ada73ca6
Danielis Joniskis <g00333859@gmit.ie>
2017-10-05 13:53:19 +0100

    added the merge sort

---
+++ 08-merge-list-sort.go
@@ -0,0 +1,17 @@
+//A function that merges two sorted lists into a new sorted list,
+//e.g. merge([1,4,6], [2,3,5]) = [1,2,3,4,5,6].
+//Author: Danielis Joniskis
+
+package main
+
+//fmt is Go’s standard Input/Output library.
+import "fmt"
+
+//A merge sort is a divide and conquer algorithm.
+//It splits the input into chunks, sorts them and then merges them together.
+
+func main(){
+	x := []int{
+		30, 88, -10, 24, 12, 5, 64, 33,  
+	}
+}
